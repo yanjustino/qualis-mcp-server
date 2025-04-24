6667047eb1208b4760d881b8219811025f5155e5
yanjustino <yanlimaj@gmail.com>
2025-04-24 20:35:10 -0300

    Refactor QualisTool methods for improved clarity and consistency

--- src/QualisMcpServer/Tools.cs
+++ src/QualisMcpServer/Tools.cs
@@ -4,16 +4,21 @@ namespace QualisMcpServer;
 public static class QualisTool
 {
     [McpServerTool, Description("Returns the classification of events for 2017-2020 - computer science")]
-    public static string About() =>
-        """
-        CLASSIFICATION OF EVENTS FOR 2017-2020 – COMPUTER SCIENCE
-        The Qualis, Qualis-Journals or Qualis/CAPES is a Brazilian system for evaluating journals, maintained by the
-        Coordination for the Improvement of Higher Education Personnel (CAPES), which relates and classifies the
-        vehicles used for the dissemination of the intellectual production of postgraduate programs of the type "stricto
-        sensu" (master's and doctoral), regarding the scope of circulation (local, national or international) and quality
-        (A, B, C), by area of evaluation.
-        """;
+    public static string About()
+        => """
+           CLASSIFICATION OF EVENTS FOR 2017-2020 – COMPUTER SCIENCE
+           The Qualis, Qualis-Journals or Qualis/CAPES is a Brazilian system for evaluating journals, maintained by the
+           Coordination for the Improvement of Higher Education Personnel (CAPES), which relates and classifies the
+           vehicles used for the dissemination of the intellectual production of postgraduate programs of the type "stricto
+           sensu" (master's and doctoral), regarding the scope of circulation (local, national or international) and quality
+           (A, B, C), by area of evaluation.
+           """;
 
     [McpServerTool, Description("Get the classification of events for 2017-2020 - computer science")]
-    public static string Qualis(string sigla) => ToolsDataSet.Content;
+    public static string AllQualis()
+        => ToolsDataSet.Content;
+
+    [McpServerTool, Description("Get the classification of events for 2017-2020 - computer science")]
+    public static string Qualis(string sigla)
+        => ToolsDataSet.Content.Split('\n').FirstOrDefault(line => line.Contains(sigla)) ?? "Qualis not found";
 }
